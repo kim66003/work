ae710fc209ee7ee538cb4cf1fe595a2598486c2a
kim66003 <kim66003@gmail.com>
2019-05-24 10:11:03 +0200

    workworkwork

--- code/helpers.js
+++ code/helpers.js
@@ -14,6 +14,12 @@ function categorizeAnswers(params) {
     var cat4 = 0;
     var dictValues = {};
     var arrayRows = [];
+    var dictCats2 = {
+      quality: 0,
+      justice: 0,
+      access: 0,
+      learning: 0
+    }
 
     // check for every answer to which category it belongs and add to count (cat1-4)
     for (var key in allQuestions) {
@@ -22,15 +28,22 @@ function categorizeAnswers(params) {
             var qValue = question.value;
             if (qValue) {
                 question.choices.forEach(function(choice) {
-                    if (choice.value === qValue || qValue.includes(choice.value)) {
-                        if (choice.score == 1) {
+                  var cValue = Number(choice.value)
+                  var cValueStr = cValue.toString()
+                  var cScore = choice.score
+                    if (cValueStr === qValue || qValue.includes(cValueStr)) {
+                        if (cScore === 1) {
                             cat1 += 1;
-                        } else if (choice.score == 2) {
+                            dictCats2.quality += 1;
+                        } else if (cScore === 2) {
                             cat2 += 1;
-                        } else if (choice.score == 3) {
+                            dictCats2.justice += 1;
+                        } else if (cScore === 3) {
                             cat3 += 1;
-                        } else if (choice.score == 4) {
+                            dictCats2.access += 1;
+                        } else if (cScore === 4) {
                             cat4 += 1;
+                            dictCats2.learning += 1;
                         }
                     }
                 });
@@ -52,7 +65,7 @@ function categorizeAnswers(params) {
     if (keys.length === 24) {
       for (i = 0; i < arrayRows.length; i++) {
         var key = Number(arrayRows[i].value)
-        var score = arrayRows[i].score
+        var score = Number(arrayRows[i].score)
         var value = Number(dictValues[key])
         if (score === 1 && (value === 4 || value === 5)) {
           cat1 += 1;
@@ -65,7 +78,6 @@ function categorizeAnswers(params) {
         }
       }
     }
-
     // make list of category counts and get the highest score
     var allCats = [cat1, cat2, cat3, cat4]
     var dictCats = {
@@ -100,18 +112,17 @@ function showText(persp) {
   var x = document.getElementsByClassName(persp);
   for (i = 0; i < x.length; i++) {
     if (x[i].style.display === "block") {
-      console.log(x[i].style.display)
       x[i].style.display = "none";
     } else {
-      console.log(x[i].style.display)
       x[i].style.display = "block";
     }
   }
 }
 
 function showInitialText(perspectives) {
+  console.log(perspectives)
   var result = categorizeAnswers(0)
-  if (result == 1) {
+  if (result === 1) {
     var x = document.getElementsByClassName(perspectives[0]);
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "block";
@@ -132,4 +143,93 @@ function showInitialText(perspectives) {
       x[i].style.display = "block";
     }
   }
+}
+
+function processResults(survey) {
+  var results = [];
+  var count = 0;
+  var questions = survey.questionHashes.names;
+  for (var key in questions) {
+    var question = questions[key][0]
+    var displayValue = question.displayValue
+    if (displayValue) {
+          count += 1;
+          var title = "\nVraag " + count.toString() + ": " + question.title + "\nAntwoord:"
+          results.push(title)
+          results.push(JSON.stringify(displayValue))
+        }
+  }
+  return results;
+}
+
+function splitByWords (text) {
+  // split string by spaces (including spaces, tabs, and newlines)
+  var wordsArray = text.split(/\s+/);
+  return wordsArray;
+}
+
+function countPerspectives (wordsMap) {
+
+  var wordsQuality = ["kwaliteit", "kwalitatief", "competentie", "kwaliteiten", "capaciteit", "bekwaamheid", "geschikt", "expertise", "competitie"]
+  var wordsJustice = ["rechtvaardigheid", "rechtvaardig", "eerlijkheid", "eerlijk", "integriteit"]
+  var wordsAccess = ["toegang", "toegankelijkheid"]
+  var wordsLearning = ["leren", "leergierig", "geleerd"]
+
+  var perspectives = {
+    quality: 0,
+    justice: 0,
+    access: 0,
+    learning: 0
+  }
+
+  for (var key in wordsMap) {
+    if (wordsQuality.includes(key)) {
+      perspectives.quality += wordsMap[key]
+    } else if (wordsJustice.includes(key)) {
+      perspectives.justice += wordsMap[key]
+    } else if (wordsAccess.includes(key)) {
+      perspectives.access += wordsMap[key]
+    } else if (wordsLearning.includes(key)) {
+      perspectives.learning += wordsMap[key]
+    }
+  }
+
+  return perspectives
+
+}
+
+function createWordMap (wordsArray) {
+
+  // create map for word counts
+  var wordsMap = {};
+
+  wordsArray.forEach(function (key) {
+      if (wordsMap.hasOwnProperty(key)) {
+        wordsMap[key]++;
+      } else {
+        wordsMap[key] = 1;
+      }
+  });
+
+  return wordsMap;
+
+}
+
+function sortByCount (wordsMap) {
+
+  // sort by count in descending order
+  var finalWordsArray = [];
+  finalWordsArray = Object.keys(wordsMap).map(function(key) {
+    return {
+      name: key,
+      total: wordsMap[key]
+    };
+  });
+
+  finalWordsArray.sort(function(a, b) {
+    return b.total - a.total;
+  });
+
+  return finalWordsArray;
+
 }
--- code/randomanswers.js
+++ code/randomanswers.js
@@ -53,16 +53,19 @@ function make_dict(list) {
     return dict
 }
 
-// arrays of statements from 4 categories
-var cat_1 = ["1...omdat het onze sociale verantwoordelijkheid is om een afspiegeling te zijn van de maatschappij", "1...als kwaliteitsdoelen maar eerst gewaarborgd zijn", "1...omdat wij als organisatie moeten inspelen op het diverser worden van de markt", "1...omdat teamleden met verschillende culturele achtergronden elkaar versterken", "1...omdat onze klantengroepen ook steeds diverser worden", "1...zolang het maar niet ten koste gaat van het maximaliseren van onze prestaties"]
-var cat_2 = ["2...omdat het ons helpt als organisatie innovatief te zijn ", "2...omdat wij als organisatie rechtvaardig moeten zijn", "2...omdat het helpt medewerkers te laten reflecteren op het eigen handelen", "2...omdat iemands culturele achtergrond er niet toe doet in het werk, het zijn kwalificaties die tellen", "2...omdat het matchen van medewerkers met klanten/leveranciers op basis van culturele achtergrond ons marktaandeel kan vergroten", "2...omdat wij als organisatie iedereen gelijke kansen moeten bieden"]
-var cat_3 = ["3...omdat wij medewerkers met een achterstandspositie extra moeten ondersteunen", "3...omdat wij geen onderscheid moeten maken op basis van culturele achtergrond", "3...omdat het ons helpt om tunnelvisie in teams tegen te gaan", "3...omdat kennis en ervaring uit een bepaalde cultuur ons helpt om producten en services af te stemmen op klanten met die cultuur", "3...omdat beoordeling objectief moet zijn, iemands culturele achtergrond speelt hierin geen rol", "3...omdat het ons toegang geeft tot een grotere visvijver aan talent"]
-var cat_4 = ["4...omdat het ons helpt nieuwe ideeën op de werkvloer te stimuleren", "4...omdat wij culturele minderheden in de organisatie vooruit moeten helpen in hun loopbaan", "4...omdat het ons helpt om vastgeroeste processen te doorbreken", "4...omdat het uiteindelijk gaat om de beste kandidaat, ongeacht iemands culturele achtergrond", "4...omdat wij achterstandsgroepen in de samenleving een kans moeten bieden op werk", "4...omdat het ons een aantrekkelijkere werkgever maakt"]
+function makeStatements() {
+  // arrays of statements from 4 categories
+  var cat_1 = ["1...omdat het onze sociale verantwoordelijkheid is om een afspiegeling te zijn van de maatschappij", "1...als kwaliteitsdoelen maar eerst gewaarborgd zijn", "1...omdat wij als organisatie moeten inspelen op het diverser worden van de markt", "1...omdat teamleden met verschillende culturele achtergronden elkaar versterken", "1...omdat onze klantengroepen ook steeds diverser worden", "1...zolang het maar niet ten koste gaat van het maximaliseren van onze prestaties"]
+  var cat_2 = ["2...omdat het ons helpt als organisatie innovatief te zijn ", "2...omdat wij als organisatie rechtvaardig moeten zijn", "2...omdat het helpt medewerkers te laten reflecteren op het eigen handelen", "2...omdat iemands culturele achtergrond er niet toe doet in het werk, het zijn kwalificaties die tellen", "2...omdat het matchen van medewerkers met klanten/leveranciers op basis van culturele achtergrond ons marktaandeel kan vergroten", "2...omdat wij als organisatie iedereen gelijke kansen moeten bieden"]
+  var cat_3 = ["3...omdat wij medewerkers met een achterstandspositie extra moeten ondersteunen", "3...omdat wij geen onderscheid moeten maken op basis van culturele achtergrond", "3...omdat het ons helpt om tunnelvisie in teams tegen te gaan", "3...omdat kennis en ervaring uit een bepaalde cultuur ons helpt om producten en services af te stemmen op klanten met die cultuur", "3...omdat beoordeling objectief moet zijn, iemands culturele achtergrond speelt hierin geen rol", "3...omdat het ons toegang geeft tot een grotere visvijver aan talent"]
+  var cat_4 = ["4...omdat het ons helpt nieuwe ideeën op de werkvloer te stimuleren", "4...omdat wij culturele minderheden in de organisatie vooruit moeten helpen in hun loopbaan", "4...omdat het ons helpt om vastgeroeste processen te doorbreken", "4...omdat het uiteindelijk gaat om de beste kandidaat, ongeacht iemands culturele achtergrond", "4...omdat wij achterstandsgroepen in de samenleving een kans moeten bieden op werk", "4...omdat het ons een aantrekkelijkere werkgever maakt"]
 
-var length_cat = cat_1.length // integer length of array category
-var cat_list = [cat_1, cat_2, cat_3, cat_4]; // list of 4 categories with statements
+  var length_cat = cat_1.length // integer length of array category
+  var cat_list = [cat_1, cat_2, cat_3, cat_4]; // list of 4 categories with statements
+  var listdict = make_dict(cat_list) // make dictionary of statements and corresponding values (1-4)
 
-var listdict = make_dict(cat_list) // make dictionary of statements and corresponding values (1-4)
+  return [listdict, length_cat]
+}
 
 // make all possible combinations of two categories from 1-4
 function answercombinations(statements, length) {
